d6b16536089891e82db501bc21a654da0f8faecf
Michael Bi <michaelhanbi@gmail.com>
2017-08-29 19:49:16 -0700

    Updated storyline

---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroController : MonoBehaviour {
+
+	public Button yesBtn;
+	public Button yes2Btn;
+	public GameObject prologue1;
+	public GameObject prologue2;
+	public GameObject prologue3;
+	public GameObject mammon;
+
+	int steps = 0;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = yesBtn.GetComponent<Button> ();
+		Button btn2 = yes2Btn.GetComponent<Button> ();
+		btn.onClick.AddListener (acceptDeal);
+		btn2.onClick.AddListener (acceptDeal);
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown (0)){
+			steps++;
+			if (steps == 1) {
+				prologue1.SetActive (false);
+				prologue2.SetActive (true);
+				mammon.SetActive (true);
+			} else if (steps == 3) {
+				SceneManager.LoadScene ("Tutorial");
+			}
+		}
+	}
+
+	void acceptDeal() {
+		//Set screen to black mammon disappears
+		mammon.SetActive (false);
+		prologue2.SetActive (false);
+		prologue3.SetActive (true);
+	}
+}
